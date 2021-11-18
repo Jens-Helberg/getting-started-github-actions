bb2a50e593f0db30fd91b5a4205a3ff2a815f472
Jens-Helberg <75250072+Jens-Helberg@users.noreply.github.com>
2021-11-18 16:18:02 +0100

    Create github-actions-script.sh

---
+++ github-actions-script.sh
@@ -0,0 +1,18 @@
+echo What is in this directory
+ls -a
+echo Current directory?
+pwd
+echo Is Java installed?
+java -version
+echo Is .NET Core installed?
+dotnet --info
+echo Is GIT installed?
+git --version
+echo What about build tools?
+mvn --version
+gradle --version
+ant -version
+env
+echo Whoami?
+whoami
+          
